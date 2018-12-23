9d7735d412869593c895a9114ebd362925b79e3b
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2018-12-23 17:13:29 +0530

    Begin Setting up Proxy configurations

--- torgod.py
+++ torgod.py
@@ -3,8 +3,11 @@
 # Works only for linux based systems
 
 import os
+import sys
+import time
 
 def logo():
+	clearscreen()
 	x='''                                                                                                                        
                                                                                                                 
 			TTTTTTTTTTTTTTTTTTTTTTT                                               GGGGGGGGGGGGG                             d::::::d
@@ -35,10 +38,64 @@ def logo():
 def clearscreen():
 	os.system('clear');
 
+def exitfn():
+	exit(0)
+
+def torconnect():
+	#code for tor connect
+
+def tordisconnect():
+	#code for tor disconnect
+
+def t():
+	current_time=time.localtime()
+	ctime = time.strftime('%H:%M:%S', current_time)
+	return "["+ ctime + "]"
+
+def ip():
+	while True:
+		try:
+			ipadd = commands.getstatusoutput('wget -qO- https://check.torproject.org | grep -Po "(?<=strong>)[\d\.]+(?=</strong)"')
+		except :
+			continue
+		break
+	return ipadd[1]
+
+TorrcCfgString = """
+
+##/////ADDED BY ToRGOD ///
+VirtualAddrNetwork 10.0.0.0/10
+AutomapHostsOnResolve 1
+TransPort 9040
+DNSPort 53
+ControlPort 9051
+
+
+"""
+
+resolvString = "nameserver 127.0.0.1"
+
+Torrc = "/etc/tor/torrc"
+resolv = "/etc/resolv.conf"
+
+
 def main():
 	clearscreen()
 	logo()
 	print('Tor anonymizer')
-	opt1=input('\n Choose an option \n\n 1)	Connect to Tor\n 2)	Disconnect From Tor \n')
+	opt1=input('\n Choose an option \n\n 1)	Connect to Tor\n 2)	Disconnect From Tor \n	3)	Exit')
+	if opt1=='1':
+		clearscreen()
+		torconnect()
+
+	elif opt1=='2':
+		clearscreen()
+		tordisconnect()
+
+	elif opt1=='3':
+		clearscreen()
+		exitfn()
+
+	else
 
 main()
